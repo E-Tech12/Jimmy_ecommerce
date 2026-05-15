78b0160fb193878c5be723a53197f04f92aeb048
E-Tech12 <adediraneric592@gmail.com>
2026-05-15 05:54:21 -0400

    prep for prod v2

--- api/index.py
+++ api/index.py
@@ -1,5 +1,10 @@
+import os
+import sys
+
+# Add the parent directory to sys.path so we can import app.py
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import app
 
-# This is the entry point for Vercel
-if __name__ == "__main__":
-    app.run()
+# Vercel needs the app variable to be available
+# We can also rename it to 'handler' if needed, but 'app' usually works
